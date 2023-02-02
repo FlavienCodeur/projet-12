3e79b1eef45bc173e8572fa5683a803a85956cde
FlavienCodeur <92309006+FlavienCodeur@users.noreply.github.com>
2023-02-02 08:08:21 +0100

    update

--- api_contracts/views.py
+++ api_contracts/views.py
@@ -24,6 +24,10 @@ class ContratListes(generics.ListCreateAPIView):
             client_contrat_assigne = [event.client.id for event in Events.objects.filter(support_contact=self.request.user)]
             return Contracts.objects.filter(client__in=client_contrat_assigne)
 
+class ContratCreer(CreateAPIView):
+
+    permission_classes = [IsAuthenticated, VendeursPermissions]
+
     def create(self, request, *args, **kwargs):
         serialiser = ContratSerialiser(data=request.data)
         if serialiser.is_valid():
@@ -32,6 +36,30 @@ class ContratListes(generics.ListCreateAPIView):
             serialiser.save(sales_contact=self.request.user)
             return Response(serialiser.data, status=status.HTTP_201_CREATED)
         return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ContratFiltre(generics.ListCreateAPIView):
+    filter_fields = ('client__last_name', 'client__email', 'payment_due', 'amount')
+    permission_classes = [IsAuthenticated, ]
+    serializer_class = ContratSerialiser
+
+    def get_queryset(self):
+        if self.request.user.assignment.department == 'Sales':
+            return Contracts.objects.filter(sales_contact=self.request.user)
+        elif self.request.user.assignment.department == 'Support':
+            contrats_clients = [event.client.id for event in Events.objects.filter(support_contact=self.request.user)]
+            return Contracts.objects.filter(client__in=contrats_clients)
+
+
+    def list(self, request, *args, **kwargs):
+        listes = self.filter_queryset(self.get_queryset())
+        try:
+            client = Customers.objects.filter(id=listes[0].client_id)
+        except IndexError:
+            return Response({"erreur": "Pas de contrats existants"},status=status.HTTP_404_NOT_FOUND)
+        self.check_object_permissions(self.request, client)
+        serialiser = self.get_serializer(listes, many=True)
+        return Response(serialiser.data)
 
 
 class ContratCorrectif(APIView):
@@ -46,8 +74,3 @@ class ContratCorrectif(APIView):
             serialiser.save()
             return Response(serialiser.data)
         return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
